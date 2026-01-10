92b63f2da7248331c17d1a1d9cec4533b8ea183c
John Rasine Irem <rajonirem8@gmail.com>
2026-01-10 13:59:11 +0100

    Implement rewrites in Next.js configuration for dashboard subdomain routing and update package-lock.json with dependency version changes.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'dashboard.middleton.ng',
+          },
+        ],
+        destination: '/dashboard/:path*',
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'dashboard.localhost',
+          },
+        ],
+        destination: '/dashboard/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
